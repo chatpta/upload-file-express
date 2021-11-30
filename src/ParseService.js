66eb954a5a9c89aa34e3c53957227b25042bfa26
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-30 11:50:14 -0500

    function is broken into multiple small ones.

--- src/ParseService.js
+++ src/ParseService.js
@@ -6,37 +6,57 @@
 
 const multer = require( 'multer' );
 
+/**
+ * Returns a multer function to parse single file.
+ * @param fieldNameCarryingFiles
+ * @return {*}
+ */
+function parseSingleFileFrom( fieldNameCarryingFiles ) {
 
-function parseSingleFileFrom( fieldName ) {
-
-    const storage = multer.memoryStorage();
-    const fileSize = { fileSize: 6 * 1024 * 1024 };
-    const fileFilter = function fileFilter( req, file, callBackFunction ) {
-        let type = file.mimetype;
-        let typeArray = type.split( "/" );
-        if ( typeArray[ 0 ] === "image" &&
-            ( typeArray[ 1 ] === "png" ||
-                typeArray[ 1 ] === "jpg" ||
-                typeArray[ 1 ] === "jpeg"
-            ) ) {
-            callBackFunction( null, true );
-        } else {
-            callBackFunction( null, false );
-        }
-    };
-    const parseFromField = multer( {
-        storage: storage,
-        limits: fileSize,
-        fileFilter: fileFilter,
+    const parseFromFieldMulter = multer( {
+        storage: multer.memoryStorage(),
+        limits: maxAcceptableFileSizeInMB( 4 ),
+        fileFilter: acceptableTypeFileTypeFilter,
     } );
 
-    return parseFromField.single( fieldName );
+    return parseFromFieldMulter.single( fieldNameCarryingFiles );
+}
+
+//===================================
+// Private functions below this line.
+//===================================
+/**
+ * Max file size if 4 Mb.
+ * @param MB
+ * @return {{fileSize: number}}
+ */
+function maxAcceptableFileSizeInMB( MB ) {
+    if ( Number.isInteger( MB ) && MB >= 0 && MB <= 4 ) {
+        return { fileSize: MB * 1024 * 1024 }
+    } else {
+        return { fileSize: 4 * 1024 * 1024 }
+    }
 }
 
 /**
- * Create upload directory if not present already
+ * Decides acceptable file types.
+ * @param req
+ * @param file
+ * @param callBackFunction
  */
-
+function acceptableTypeFileTypeFilter( req, file, callBackFunction ) {
+    let type = file.mimetype;
+    let typeArray = type.split( "/" );
+    if ( typeArray[ 0 ] === "image" &&
+        ( typeArray[ 1 ] === "png" ||
+            typeArray[ 1 ] === "jpg" ||
+            typeArray[ 1 ] === "jpeg"
+        ) ) {
+        callBackFunction( null, true );
+    } else {
+        callBackFunction( null, false );
+    }
+}
 
 module.exports = {
     parseSingleFileFrom
