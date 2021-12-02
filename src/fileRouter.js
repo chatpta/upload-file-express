de34e53bb041aaa1c328413943a9fa32812cc97f
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-12-02 13:00:12 -0500

    pdf uploading

--- src/fileRouter.js
+++ src/fileRouter.js
@@ -34,12 +34,9 @@ fileRouter.post( '/profile', upload.single( 'profile' ), function ( req, res ) {
 } );
 
 fileRouter.post( '/pdf', upload.single( 'cv' ), function ( req, res ) {
-    // req.file is the name of your file in the form above, here 'uploaded_file'
-    // req.body will hold the text fields, if there were any
     res.setHeader( 'Content-Type', 'application/json' );
     res.json( { message: req.file.originalname + ' is uploaded successfully' });
 } );
-
 
 
 // GET AVTAR
